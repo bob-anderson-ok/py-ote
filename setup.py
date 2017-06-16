351a301674721d2aaf0b5cfbb01d56f6f5187d4b
bob.anderson.ok@gmail.com <bob.anderson.ok@gmail.com>
2017-06-16 13:09:02 -0700

    Changed version
    Signed-off-by: bob.anderson.ok@gmail.com <bob.anderson.ok@gmail.com>

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
         description='pyote is a simplified subset of R-OTE',
         license='License :: OSI Approved :: MIT License',
         url=r'https://github.com/bob-anderson-ok/py-ote',
-        version='1.4.dev0',
+        version='1.5.dev0',
         author='Bob Anderson',
         author_email='bob.anderson.ok@gmail.com',
         maintainer='Bob Anderson',
