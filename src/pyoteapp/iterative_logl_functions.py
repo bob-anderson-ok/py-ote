5.2.3 Fixes flash edge issue and restores 6 digit precision to timeDelta
Signed-off-by: boban <bob.anderson.ok@gmail.com>

src/pyoteapp/iterative_logl_functions.py
@@ -219,11 +219,11 @@ def find_best_r_only_from_min_max_size(
 
     # Use numpy version of metric calculator to initialize iteration variables
 
-    # changed in 4.4.7
-    # b_s, b_s2, b_n, b_var = calc_metric_numpy(y[r + 1:right + 1])
-    b_s, b_s2, b_n, b_var = calc_metric_numpy(y[r:right + 1])
+    # B excludes y[r] and the extra point at right - but this is meaningless because r is incorrect at this point
+    b_s, b_s2, b_n, b_var = calc_metric_numpy(y[r + 1:right])
 
-    a_s, a_s2, a_n, a_var = calc_metric_numpy(y[left:r])
+    # A always excludes y[r]) - but this is meaningless because r is incorrect at this point
+    a_s, a_s2, a_n, a_var = calc_metric_numpy(y[left + 1:r])
 
     b = b_s / b_n
     a = a_s / a_n
@@ -232,17 +232,15 @@ def find_best_r_only_from_min_max_size(
     metric = calc_metric()
     update_best_solution()
 
-    r_final = left + max_event
+    r_final = left + max_event - 1
 
     while r < r_final:
         # calc metric for next r position from current position
 
-        # changed in 4.4.7
-        # b_s, b_s2, b_n, b_var = sub_entry(y[r+1], b_s, b_s2, b_n, True)
-        b_s, b_s2, b_n, b_var = sub_entry(y[r], b_s, b_s2, b_n, True)
-
         a_s, a_s2, a_n, a_var = add_entry(y[r], a_s, a_s2, a_n, True)
         r += 1
+        b_s, b_s2, b_n, b_var = sub_entry(y[r], b_s, b_s2, b_n, True)
+
 
         metric = calc_metric()
         b = b_s / b_n
@@ -314,18 +312,15 @@ def find_best_d_only_from_min_max_size(
     d = right - max_event  # Initial d position
 
     # Use numpy version of metric calculator to initialize iteration variables
+
+    # Excludes d from B - this is meaningless because d is incorrect at this point
     b_s, b_s2, b_n, b_var = calc_metric_numpy(y[left:d])
 
-    # changed in 4.4.7
-    # a_s, a_s2, a_n, a_var = calc_metric_numpy(y[d+1:right+1])
-    # a_s, a_s2, a_n, a_var = calc_metric_numpy(y[d:right+1])
-    # changed in 4.4.8
-    a_s, a_s2, a_n, a_var = calc_metric_numpy(y[d:right])
+    # Excludes d from A - this is meaningless because d is incorrect at this point
+    a_s, a_s2, a_n, a_var = calc_metric_numpy(y[d+1:right])
 
     b = b_s / b_n
     a = a_s / a_n
-
-    # print(b, a, b_n, a_n)
 
     # Calculate metric for initial position of d
     metric = calc_metric()
@@ -341,9 +336,11 @@ def find_best_d_only_from_min_max_size(
 
         # changed in 4.4.7
         # a_s, a_s2, a_n, a_var = sub_entry(y[d+1], a_s, a_s2, a_n, True)
-        a_s, a_s2, a_n, a_var = sub_entry(y[d], a_s, a_s2, a_n, True)
+        # a_s, a_s2, a_n, a_var = sub_entry(y[d], a_s, a_s2, a_n, True)
 
         d += 1
+        a_s, a_s2, a_n, a_var = sub_entry(y[d], a_s, a_s2, a_n, True)
+
 
         metric = calc_metric()
         b = b_s / b_n
@@ -485,7 +482,6 @@ def locate_fixed_event_position(
 
     return d_max, r_max, b_max, a_max, sigma_b, sigma_a, max_metric, solution_count
 
-
 @njit  # cache=True gave pickling error
 def locate_event_from_d_and_r_ranges(
         y: np.ndarray, left: int, right: int, d_start: int, d_end: int,
@@ -524,7 +520,7 @@ def locate_event_from_d_and_r_ranges(
         r = r_start
 
         if d > left:
-            b_sl, b_s2l, b_nl, b_varl = calc_metric_numpy(y[left:d])
+            b_sl, b_s2l, b_nl, b_varl = calc_metric_numpy(y[left+1:d])
             # Lefthand wing
         else:
             b_sl = 0.0
@@ -532,9 +528,7 @@ def locate_event_from_d_and_r_ranges(
             b_nl = 0
             b_varl = 0.0
 
-        # changed in 4.4.7
-        # b_sr, b_s2r, b_nr, b_varr = calc_metric_numpy(y[r+1:right+1])
-        b_sr, b_s2r, b_nr, b_varr = calc_metric_numpy(y[r:right+1])
+        b_sr, b_s2r, b_nr, b_varr = calc_metric_numpy(y[r+1:right])
 
         # Righthand wing
 
@@ -543,9 +537,7 @@ def locate_event_from_d_and_r_ranges(
         b_n = b_nl + b_nr
         b_var = b_varl + b_varr
 
-        # changed in 4.4.7
-        # a_s, a_s2, a_n, a_var = calc_metric_numpy(y[d+1:r])
-        a_s, a_s2, a_n, a_var = calc_metric_numpy(y[d:r])
+        a_s, a_s2, a_n, a_var = calc_metric_numpy(y[d+1:r])
 
         # ============== calc_metric() =================
         max_var = max(a_var, b_var, MIN_FLOAT)
@@ -588,11 +580,9 @@ def locate_event_from_d_and_r_ranges(
 
         while r < r_end:
             b_s, b_s2, b_n, b_var = sub_entry(y[r], b_s, b_s2, b_n, True)
-            # changed in 4.4.8
-            # a_s, a_s2, a_n, a_var = add_entry(y[r-1], a_s, a_s2, a_n, True)
+            r += 1
             a_s, a_s2, a_n, a_var = add_entry(y[r], a_s, a_s2, a_n, True)
 
-            r += 1
             # ============== calc_metric() =================
             max_var = max(a_var, b_var, MIN_FLOAT)
 

src/pyoteapp/noiseUtils.py
@@ -55,4 +55,9 @@ def getCorCoefs(x, y):
         for lag in range(11):
             ans.append(laggedCoef(residuals, lag))
 
-    return np.array(ans), len(x), np.std(residuals)
+    # This deals with no-noise test sets.
+    sigma = np.std(np.diff(np.array(y))) / np.sqrt(2.0)  # In place of: np.std(residuals)
+    if sigma == 0:
+        ans = [1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    return np.array(ans), len(x), sigma

src/pyoteapp/pyote.py
@@ -1205,7 +1205,7 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
                     self.showInfo(f'The reading numbers supplied are invalid.')
 
     def plotFamilyOfLightcurves(self):
-        def move_figure(f, x, y):
+        def move_figure(f, x, y):  # noqa
             backend = matplotlib.get_backend()
             if backend == 'TkAgg':
                 f.canvas.manager.window.wm_geometry("+%d+%d" % (x,y))
@@ -1214,18 +1214,18 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
             else:
                 f.canvas.manager.window.move(x,y)
 
-        def plot_curve_set(calculate_curves, x, yOut):
+        def plot_curve_set(calculate_curves, x, yOut):  # noqa
             fig = plt.figure(constrained_layout=False, figsize=(10, 8))
             fig.canvas.manager.set_window_title("Family plot")
-            ax1 = fig.add_subplot(1, 1, 1)  # lightcurve axes
+            ax1 = fig.add_subplot(1, 1, 1)  # noqa lightcurve axes
 
             self.showMsg("", blankLine=False)
             if calculate_curves:
                 self.showMsg(f'Initiating computations ...', bold=True, color='red', blankLine=False)
-            for i in range(1, numCurves + 1):
+            for i in range(1, numCurves + 1):  # noqa
                 if calculate_curves:
                     QtWidgets.QApplication.processEvents()
-                    x, y, D_edge, R_edge = generalizedDiffraction(LCP=self.Lcp, wavelength1=None, wavelength2=None,
+                    x, y, D_edge, R_edge = generalizedDiffraction(LCP=self.Lcp, wavelength1=None, wavelength2=None,  # noqa
                                                                   skip_central_calc=False)
                     if i == 1:
                         ax1.set_ylim(0, 1.1 * np.max(y))
@@ -1344,7 +1344,6 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
             referenceCurveNumber = numCurves // 2 + 1
 
             noiseValues = []
-            noiseValue = None
             gotNoiseValue = True
             while gotNoiseValue:
                 if not noiseValues:
@@ -1367,7 +1366,6 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
 
             sampled_y = [[] for _ in range(numCurves)]
             xpts = []
-            i = 0
             sample_index = -sample_index_step
             i_limit = len(yVec[0])
             for k in range(numCurves):
@@ -2331,42 +2329,6 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
 
         self.processModelLightcurveCoreEdit()
 
-    # TODO Remove this experimental code
-    def sampleFamilyLightCurve(self, x_values_km, y_values):
-        # Build interpolation function. This is done to deal with the finite resolution of
-        # the 2048 point lightcurve
-        x_values_sec = x_values_km / self.Lcp.shadow_speed
-        x_values_sec -= x_values_sec[0]
-        tObsStart = convertTimeStringToTime(self.yTimes[0])
-        x_values_sec += self.modelTimeOffset + tObsStart
-
-        interpolator = interpolate.interp1d(x_values_sec, y_values)
-
-        sample_time = tObsStart
-
-        # x_vals will be reading numbers
-        x_vals = []
-        y_vals = []
-
-        while sample_time <= x_values_sec[-1]:
-            if sample_time >= x_values_sec[0]:
-                # Compute x_vals as reading number
-                x_vals.append(round((sample_time - tObsStart) / self.Lcp.frame_time))
-                y_vals.append(interpolator(sample_time))
-            sample_time += self.Lcp.frame_time
-            # This keeps us from from sampling the model curve past
-            # the end of the observation
-            if len(x_vals) >= self.dataLen:
-                break
-
-        if len(x_vals) == 0:
-            breakpoint()
-
-        x_samples = np.array(x_vals)
-        y_samples = np.array(y_vals)
-
-        return x_samples, y_samples
-
     def sampleModelLightcurve(self):
         # Build interpolation function. This is done to deal with the finite resolution of
         # the 2048 point lightcurve
@@ -2454,18 +2416,6 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
 
         matchingObsYvalues = np.array(matchingObsYvalues)
         modelYsamples = np.array(modelYsamples)
-
-        # TODO Experimental code to measure uncertainty in metric
-        # metrics = []
-        # test_noise = modelSigmaB * 0.2
-        # num_trials = 4000
-        # for i in range(num_trials):
-        #     renoisedObs = matchingObsYvalues + np.random.normal(0, test_noise, matchingObsYvalues.size)
-        #     noisedMetric = np.sum(((modelYsamples - renoisedObs) / np.sqrt(modelSigmaB**2 + test_noise**2))**2) / modelYsamples.size
-        #     metrics.append(noisedMetric)
-        #
-        # metrics = np.array(metrics)
-        # self.showMsg(f'mean metric: {np.mean(metrics):0.4f}   std metrics: {np.std(metrics):0.4f}')
 
         # We produce these values as a side effect - only used during edge-on-disk (penumbral) fits
         self.dMetric = np.sum(((modelYsamples[0:center_frame] - matchingObsYvalues[0:center_frame]) / modelSigmaB)**2) / modelYsamples.size
@@ -7780,14 +7730,18 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
             self.corCoefs = []
 
         if D and R:
-            self.sigmaA = None
-            # self.corCoefs = []
+            # D - 1 is specified so that the D point is excluded from the noise and B level calculations
+            self.processBaselineNoiseFromIterativeSolution(self.left, D - 1)  # Left side
+            newBleft = self.B  # This is filled in by the above call
 
-            self.processBaselineNoiseFromIterativeSolution(self.left, D - 1)
+            # R + 1 is specified so that the R point is excluded from the noise and B level calculations
+            self.processBaselineNoiseFromIterativeSolution(R + 1, self.right)  # Right side
+            newBright = self.B
 
-            self.processBaselineNoiseFromIterativeSolution(R, self.right)
+            self.B = (newBleft + newBright) / 2
 
-            self.processEventNoiseFromIterativeSolution(D, R - 1)
+            # D + 1 and R -1 are used to excluded the D and R points from noise and A and B values
+            self.processEventNoiseFromIterativeSolution(D + 1, R - 1)
 
             # Try to warn user about the possible need for block integration by testing the lag 1
             # and lag 2 correlation coefficients.  The tests are just guesses on my part, so only
@@ -7812,21 +7766,11 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
             if self.sigmaA is None:
                 self.sigmaA = self.sigmaB
         elif D:
-            self.sigmaA = None
-
             self.processBaselineNoiseFromIterativeSolution(self.left, D - 1)
-
-            self.processEventNoiseFromIterativeSolution(D, self.right)
-            if self.sigmaA is None:
-                self.sigmaA = self.sigmaB
+            self.processEventNoiseFromIterativeSolution(D+1, self.right)
         else:  # R only
-            self.sigmaA = None
-
-            self.processBaselineNoiseFromIterativeSolution(R, self.right)
-
+            self.processBaselineNoiseFromIterativeSolution(R+1, self.right)
             self.processEventNoiseFromIterativeSolution(self.left, R - 1)
-            if self.sigmaA is None:
-                self.sigmaA = self.sigmaB
 
         self.prettyPrintCorCoefs()
 
@@ -8090,9 +8034,9 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
                 blankLine=False)
             self.displaySolution(subframe=False)  # First solution
 
-            # This fills in self.sigmaB and self.sigmaA (incorrectly) but is useful
+            # This fills in self.sigmaB and self.sigmaA and self.B and self.A Also, it is useful
             # because it tests correlation coefficients to warn of the need for block integration
-            self.extract_noise_parameters_from_iterative_solution()
+            self.extract_noise_parameters_from_iterative_solution()  # This does proper exclusions at D and R
 
             DfitMetric = RfitMetric = 0.0
 
@@ -8111,7 +8055,7 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
 
             # Here is where we get sigmaB and sigmaA with the transition points excluded
             subDandR, new_b, new_a, newSigmaB, newSigmaA = subFrameAdjusted(
-                eventType=self.eventType, cand=(d, r), B=b, A=a,
+                eventType=self.eventType, cand=(d, r), B=self.B, A=self.A,
                 sigmaB=self.sigmaB, sigmaA=self.sigmaA, yValues=self.yValues,
                 left=self.left, right=self.right)
 
@@ -8944,9 +8888,6 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
 
     def processEventNoiseFromIterativeSolution(self, left, right):
 
-        if (right - left) < 9:
-            return
-
         assert left >= self.left
         assert right <= self.right
 
@@ -8955,6 +8896,9 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
         for i in range(left, right + 1):
             self.eventXvals.append(i)
             self.eventYvals.append(self.yValues[i])
+
+        # Recalculate A with D and R points excluded
+        self.A = np.mean(self.eventYvals)
 
         _, self.numNApts, self.sigmaA = getCorCoefs(self.eventXvals,
                                                     self.eventYvals)
@@ -9048,6 +8992,9 @@ class SimplePlot(PyQt5.QtWidgets.QMainWindow, gui.Ui_MainWindow):
         for i in range(left, right + 1):
             self.baselineXvals.append(i)
             self.baselineYvals.append(self.yValues[i])
+
+        # Recalculate baseline with D and R points excluded
+        self.B = np.mean(self.baselineYvals)
 
         if not self.userDeterminedBaselineStats:
             self.newCorCoefs, self.numNApts, sigB = getCorCoefs(self.baselineXvals,

src/pyoteapp/solverUtils.py
@@ -15,9 +15,10 @@ from numba import njit
 
 
 def aicModelValue(*, obsValue=None, B=None, A=None, sigmaB=None, sigmaA=None):
-    assert(B >= A)
-    assert(sigmaA > 0.0)
-    assert(sigmaB > 0.0)
+    assert B >= A
+    # assert sigmaA > 0.0
+    # assert sigmaB > 0.0
+
     # This function determines if an observation point should categorized as a baseline (B)
     # point, an event (A) point, or a valid intermediate point using the Akaike Information Criterion
     # An intermediate point reflects a more complex model (higher dimension model)
@@ -27,6 +28,13 @@ def aicModelValue(*, obsValue=None, B=None, A=None, sigmaB=None, sigmaA=None):
         return A  # Categorize as event point
     if B == A:
         return B
+
+    # We do this to allow test files with zero noise to be processed
+    if sigmaA == 0:
+        if sigmaB == 0:
+            return obsValue
+        else:
+            assert sigmaA > 0.0
 
     sigmaM = sigmaA + (sigmaB - sigmaA) * ((obsValue - A) / (B - A))
     loglB = loglikelihood(obsValue, B, sigmaB)
@@ -251,10 +259,12 @@ def newCalcBandA(*, yValues=None, tpList=None, left=None, right=None, cand=None)
         else:
             # changed in 4.4.7
             # B = np.mean(yValues[R+1:right+1])
-            valuesToUse = [val for i, val in enumerate(yValues[R:right + 1]) if i not in tpList]
+            # Changed i to i + R in 5.2.3 to solve flash edge time problem
+            valuesToUse = [val for i, val in enumerate(yValues[R:right + 1]) if i + R not in tpList]
             B = np.mean(valuesToUse)
             sigmaB = np.std(valuesToUse)
-        valuesToUse = valuesToBeUsed(left, R)
+        # Changed to R - 1 in 5.2.3 to solve flash edge time problem
+        valuesToUse = valuesToBeUsed(left, R - 1)
         A = np.mean(valuesToUse)
         sigmaA = np.std(valuesToUse)
         if A >= B:
@@ -452,11 +462,6 @@ def subFrameAdjusted(*, eventType=None, cand=None, B=None, A=None,
     #  all B pixels have a constant value.  Limovie can produce a sigmaA=0
     # when a rectangular aperture is in use as well
 
-    if sigmaA == 0.0:
-        sigmaA = MIN_SIGMA
-    if sigmaB == 0.0:
-        sigmaB = MIN_SIGMA
-
     if eventType == 'Donly':
         if aicModelValue(obsValue=yValues[D], B=B, A=A, sigmaB=sigmaB, sigmaA=sigmaA) == yValues[D]:
             # If point at D categorizes as M (valid mid-point), do sub-frame
@@ -524,8 +529,6 @@ def subFrameAdjusted(*, eventType=None, cand=None, B=None, A=None,
             # (finishes D)
             adjD = adjustD()
             transitionPoints.append(D)
-            B, A, sigmaB, sigmaA = newCalcBandA(yValues=yValues, tpList=transitionPoints,
-                                                left=left, right=right, cand=(D, R))
         elif aicModelValue(obsValue=yValues[D], B=B, A=A, sigmaB=sigmaB, sigmaA=sigmaA) == B:
             # The point at D categorizes as B, set D to D+1 and recalculate B and A
             D = D + 1
@@ -536,8 +539,6 @@ def subFrameAdjusted(*, eventType=None, cand=None, B=None, A=None,
                     sigmaA=sigmaA) == yValues[D]:
                 adjD = adjustD()
                 transitionPoints.append(D)
-                B, A, sigmaB, sigmaA = newCalcBandA(yValues=yValues, tpList=transitionPoints,
-                                                    left=left, right=right, cand=(D, R))
         elif aicModelValue(obsValue=yValues[D-1], B=B, A=A, sigmaB=sigmaB,
                            sigmaA=sigmaA) == yValues[D-1]:
             # The point at D categorizes as A, and we have found
@@ -546,30 +547,22 @@ def subFrameAdjusted(*, eventType=None, cand=None, B=None, A=None,
             D = D - 1
             adjD = adjustD()
             transitionPoints.append(D)
-            B, A, sigmaB, sigmaA = newCalcBandA(yValues=yValues, tpList=transitionPoints,
-                                                left=left, right=right, cand=(D, R))
 
         if aicModelValue(
                 obsValue=yValues[R], B=B, A=A, sigmaB=sigmaB, sigmaA=sigmaA) == yValues[R]:
             # The point at R categorizes as M, do sub-frame adjustment
             adjR = adjustR()
             transitionPoints.append(R)
-            B, A, sigmaB, sigmaA = newCalcBandA(yValues=yValues, tpList=transitionPoints,
-                                                left=left, right=right, cand=(D, R))
         elif aicModelValue(obsValue=yValues[R], B=B, A=A, sigmaB=sigmaB, sigmaA=sigmaA) == A:
             # The point at R categorizes as A, set R to R + 1 and recalculate B and A
             R = R + 1
             adjR = R
-            B, A, sigmaB, sigmaA = newCalcBandA(yValues=yValues, tpList=transitionPoints,
-                                                left=left, right=right, cand=(D, R))
             # It's possible that this new point qualifies as M --- so we check
             if aicModelValue(
                     obsValue=yValues[R], B=B, A=A, sigmaB=sigmaB,
                     sigmaA=sigmaA) == yValues[R]:
                 adjR = adjustR()
                 transitionPoints.append(R)
-                B, A, sigmaB, sigmaA = newCalcBandA(yValues=yValues, tpList=transitionPoints,
-                                                    left=left, right=right, cand=(D, R))
         elif aicModelValue(obsValue=yValues[R - 1], B=B, A=A, sigmaB=sigmaB,
                            sigmaA=sigmaA) == yValues[R - 1]:
             # The point at R categorizes as B, and we have found
@@ -578,8 +571,6 @@ def subFrameAdjusted(*, eventType=None, cand=None, B=None, A=None,
             R = R - 1
             adjR = adjustR()
             transitionPoints.append(R)
-            B, A, sigmaB, sigmaA = newCalcBandA(yValues=yValues, tpList=transitionPoints,
-                                                left=left, right=right, cand=(D, R))
         return [adjD, adjR], B, A, sigmaB, sigmaA
 
     else:

src/pyoteapp/timestampUtils.py
@@ -50,9 +50,12 @@ def improveTimeStep(outliers, deltaTime):
     # be found.  In that case, we return a timeStep of 0.0 and let the
     # program respond to this invalid value at a higher level.
     if validIntervals:
-        # Changed in 5.0.1
+        # Changed in 5.0.1  put back in 5.2.3
         # betterTimeStep = np.mean(validIntervals)
-        betterTimeStep = np.median(validIntervals)
+        # removed in 5.2.3
+        # betterTimeStep = np.median(validIntervals)
+        betterTimeStep = np.mean(validIntervals)
+
     else:
         betterTimeStep = 0.0
     return betterTimeStep